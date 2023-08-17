1260c9340d47cd92f800f6d67c3a0385fde99865
Amey Kunte <amey.k@research.iiit.ac.in>
2023-08-17 23:13:25 +0530

    Update source.cpp

--- src/executors/source.cpp
+++ src/executors/source.cpp
@@ -27,8 +27,46 @@ bool semanticParseSOURCE()
     return true;
 }
 
-void executeSOURCE()
-{
+void executeSOURCE(const std::string& fileName) {
+    std::ifstream queryFile("data/" + fileName + ".ra");
+    if (!queryFile) {
+        std::cerr << "Error opening .ra file: " << fileName << std::endl;
+        return;
+    }
+
+    std::string command;
+    while (getline(queryFile, command)) {
+        cout << "\n> " << command << endl;
+
+        tokenizedQuery.clear();
+        parsedQuery.clear();
+        logger.log("\nReading New Command: ");
+        logger.log(command);
+
+        auto words_begin = std::sregex_iterator(command.begin(), command.end(), delim);
+        auto words_end = std::sregex_iterator();
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+            tokenizedQuery.emplace_back((*i).str());
+
+        if (tokenizedQuery.size() == 1 && tokenizedQuery.front() == "QUIT") {
+            break;
+        }
+
+        if (tokenizedQuery.empty()) {
+            continue;
+        }
+
+        if (tokenizedQuery.size() == 1) {
+            cout << "SYNTAX ERROR" << endl;
+            continue;
+        }
+
+        logger.log("doCommand");
+        if (syntacticParse() && semanticParse()) {
+            executeCommand();
+        }
+    }
+
     logger.log("executeSOURCE");
     return;
 }
